e9e49851aeceec388153dce59012218f0d57895d
Josh McConnell <josh@sanity.io>
2023-05-05 16:39:47 +0100

    Remove unused entry

--- awx/provider.go
+++ awx/provider.go
@@ -77,7 +77,6 @@ func Provider() *schema.Provider {
 		DataSourcesMap: map[string]*schema.Resource{
 			"awx_credential_azure_key_vault": dataSourceCredentialAzure(),
 			"awx_credential":                 dataSourceCredentialByID(),
-			"awx_credentials":                dataSourceCredentials(),
 			"awx_inventory_group":            dataSourceInventoryGroup(),
 			"awx_inventory":                  dataSourceInventory(),
 			"awx_job_template":               dataSourceJobTemplate(),
